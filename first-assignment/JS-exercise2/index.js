4c875719b7c9e6eb20d24b8bc4344306df749615
drae sims <draesims0@gmail.com>
2023-06-24 17:01:35 -0700

    next-lesson

--- first-assignment/JS-exercise2/index.js
+++ first-assignment/JS-exercise2/index.js
@@ -1,2 +1,12 @@
 var text = document.querySelector ("#header").textContent
 document.querySelector ("#paragraph").textContent = text 
+var text = document.querySelector ("#head1").textContent
+document.querySelector ("#paragraph1").textContent = text
+
+var header = document.getElementById("practice")
+header.style.fontsize,40
+header.style.backgroundColor = "red"
+document.body.style.backgroundColor = "lightgreen"
+console.log
+var classname = document.querySelector("#header") = classname
+console.log
